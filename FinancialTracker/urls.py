b7ad35ffeae5236a276979951714781ecbdd798a
Tyler Bloom <tylerbloom9787@gmail.com>
2020-02-10 18:29:07 -0800

    Set up REST framework and the view for the stocks

--- FinancialTracker/urls.py
+++ FinancialTracker/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stocks/', views.StockList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializer import StockSerializer
 
-# Create your views here.
+# List all the stocks in JSON format
+class StockList(APIView):
+    def get(self, request):
+        stocks = Stock.objects.all()
+        serializer = StockSerializer(stocks, many=True)
+        return Response(serializer.data)
