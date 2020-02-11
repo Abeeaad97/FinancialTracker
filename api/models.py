3bf67ebabcdd0c0a76af1a9daef1f2aff2d7b714
Tyler Bloom <tylerbloom9787@gmail.com>
2020-02-10 16:49:35 -0800

    Set up a template for stock model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Template Model
+class Stock(models.Model):
+    ticker = models.CharField(max_length=10)           # Name of the stock
+    open = models.FloatField()                         # Opening stock price
+    close = models.FloatField()                        # Closing stock price
+    volume = models.IntegerField()                     # Amount of sales
+
+    def __str__(self):
+        return self.ticker
+
